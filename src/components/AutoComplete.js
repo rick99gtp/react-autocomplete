76593a407064946f22cd0e9b94b26fa25d6af314
Rick Beyer <beyer.richard73@gmail.com>
2021-12-18 16:01:11 -0600

    try again - first commit

---
+++ src/components/AutoComplete.js
@@ -0,0 +1,12 @@
+import './AutoComplete.css';
+import InputContainer from './InputContainer';
+
+const AutoComplete = () => {
+    return (
+        <div className='autocomplete__container'>
+            <InputContainer />
+        </div>
+    )
+}
+
+export default AutoComplete;
---
+++ src/components/InputContainer.js
@@ -0,0 +1,12 @@
+import './InputContainer.css';
+
+const InputContainer = () => {
+    return (
+        <div className='input__container'>
+            <input type="text" placeholder="Type something..." />
+            
+        </div>
+    )
+}
+
+export default InputContainer;
