0d8cde8570d837aa46fc95ce5ef77e4ba75a1add
Rick Beyer <beyer.richard73@gmail.com>
2021-12-18 19:18:08 -0600

    Container now widens when search icon clicked

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,10 +1,17 @@
 import './AutoComplete.css';
 import InputContainer from './InputContainer';
+import { useState } from 'react';
 
 const AutoComplete = () => {
+    const [visible, setVisible] = useState(false);
+
+    const handleClick = () => {
+        setVisible(!visible);
+    };
+
     return (
-        <div className='autocomplete__container'>
-            <InputContainer />
+        <div className={`autocomplete__container ${visible ? 'autocomplete-width' : ''}`}>
+            <InputContainer visible={visible} changeVisible={handleClick} />
         </div>
     )
 }
--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,18 +1,12 @@
 import './InputContainer.css';
 import IconContainer from './IconContainer';
-import { useState } from 'react';
 
-const InputContainer = () => {
-    const [visible, setVisible] = useState(false);
-
-    const handleClick = () => {
-        setVisible(!visible);
-    };
+const InputContainer = ({visible, changeVisible}) => {
 
     return (
         <div className='input__container'>
-            <input className={`${visible ? 'input-visible' : ''}`} type="text" placeholder="Type something..." />
-            <IconContainer visible={visible} changeVisible={handleClick} />
+            {/* <input className={`${visible ? 'input-visible' : ''}`} type="text" placeholder="Type something..." /> */}
+            <IconContainer visible={visible} changeVisible={changeVisible} />
         </div>
     )
 }
