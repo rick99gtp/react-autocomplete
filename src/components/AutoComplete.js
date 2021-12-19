a2e5cdbd09a7add2ae9681da3074bc22102a19ed
Rick Beyer <beyer.richard73@gmail.com>
2021-12-18 19:35:20 -0600

    Input now shows after animations widen container

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,17 +1,27 @@
 import './AutoComplete.css';
 import InputContainer from './InputContainer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const AutoComplete = () => {
     const [visible, setVisible] = useState(false);
+    const [inputVisible, setInputVisible] = useState(false);
 
     const handleClick = () => {
         setVisible(!visible);
     };
 
+    useEffect(() => {
+        if(visible) {
+            const inputTimer = setTimeout(() => {
+                setInputVisible(true);
+                clearTimeout(inputTimer);
+            }, 1250);
+        }
+    },[visible]);
+
     return (
         <div className={`autocomplete__container ${visible ? 'autocomplete-width' : ''}`}>
-            <InputContainer visible={visible} changeVisible={handleClick} />
+            <InputContainer inputVisible={inputVisible} visible={visible} changeVisible={handleClick} />
         </div>
     )
 }
--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,11 +1,27 @@
 import './InputContainer.css';
 import IconContainer from './IconContainer';
+import { useEffect, useState } from 'react';
 
-const InputContainer = ({visible, changeVisible}) => {
+const InputContainer = ({visible, changeVisible, inputVisible}) => {
+
+    const [displayInput, setDisplayInput] = useState(false);
+    const [inputOpacity, setInputOpacity] = useState(false);
+
+    useEffect(() => {
+        if(inputVisible) {
+            setDisplayInput(!displayInput);
+        }
+    }, [inputVisible]);
+
+    useEffect(() => {
+        if(displayInput) {
+            setInputOpacity(true);
+        }
+    }, [displayInput]);
 
     return (
         <div className='input__container'>
-            {/* <input className={`${visible ? 'input-visible' : ''}`} type="text" placeholder="Type something..." /> */}
+            { inputVisible ? <input className={`${displayInput ? 'display-input' : ''} ${inputOpacity ? 'input-opacity' : ''}`} type="text" placeholder="Type something..." /> : null }
             <IconContainer visible={visible} changeVisible={changeVisible} />
         </div>
     )
